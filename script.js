e7b6b766a29ff7cc8d73be3e6e0b533106c01232
Alena Nemtsova <jelenatbs@gmail.com>
2024-01-19 15:17:05 +0300

    add event listeners

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+'use strict';
 
+const btn = document.querySelector('#btn');
+const inputText = document.querySelector('#text');
+const square = document.querySelector('#square');
+const circle = document.querySelector('#circle');
+const eBtn = document.querySelector('#e_btn');
+const inputRange = document.querySelector('#range');
 
+const changeColor = function () {
+    square.style.backgroundColor = inputText.value;
+}
 
+const changeCircleSize = function (event) {
+    circle.style.width = `${event.target.value}%`;
+    circle.style.height = `${event.target.value}%`;
+}
 
+eBtn.style.display = 'none';
+
+btn.addEventListener('click', changeColor);
+inputRange.addEventListener('input', changeCircleSize)
 
